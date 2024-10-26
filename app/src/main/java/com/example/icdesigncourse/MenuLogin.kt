bcb4c7677479e4016ff081343f82ecb2d321adc6
SinauAksara <gelfandalharitszakaria@gmail.com>
2024-10-26 23:59:53 +0700

    menambahkan coding

--- app/src/main/java/com/example/icdesigncourse/MenuLogin.kt
+++ app/src/main/java/com/example/icdesigncourse/MenuLogin.kt
@@ -1,12 +1,21 @@
 package com.example.icdesigncourse
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MenuLogin : AppCompatActivity() {
+    companion object {
+        var email = "icdesign@gmail.com"
+        var pass = "1234"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +24,21 @@ class MenuLogin : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val txtEmail:EditText = findViewById(R.id.isiEmail)
+        val txtPassword:EditText = findViewById(R.id.isiPassword)
+        val btnMasuk: Button = findViewById(R.id.btnMasuk)
+        val btnGoogle: Button = findViewById(R.id.btnLogin)
+
+        btnMasuk.setOnClickListener{
+            if (txtEmail.text.toString() == email &&
+                txtPassword.text.toString() == pass){
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }else{
+                Toast.makeText(this,"Login failed, Check your email and password",
+                    Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
